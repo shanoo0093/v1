80702a2bc501450e545a7a2945341d88ff498173
shanoo0093 <71050916+shanoo0093@users.noreply.github.com>
2020-09-10 13:18:42 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -2,8 +2,8 @@
 include_once("sqlConn.php");
 $con -> select_db("my_db");
 
-if(isset($_GET['id']) && $_GET['id']!=""){
-    $sql=sprintf("DELETE FROM persons WHERE personID=%s", $_GET['id']);
+if(isset($_GET['account']) && $_GET['account']!=""){
+    $sql=sprintf("DELETE FROM persons WHERE Account=%s", $_GET['account']);
 
     $con -> query($sql);
     header("location:index.php");
