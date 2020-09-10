f49a62fc5238e9ff5fe326ef19f0872c520defc1
shanoo0093 <71050916+shanoo0093@users.noreply.github.com>
2020-09-10 15:27:47 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -7,12 +7,12 @@ if(isset['account'] != null)
         $sql = "delete from my_db where Account='$Account'";
        
                 echo '刪除成功!';
-                header("location:index.php");
+                echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
 }
         else
         {
                echo '刪除失敗!';
-               header("location:index.php");
+               echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
         }
 }
 
