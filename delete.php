db30316fecce8ccd17fe36b263a834507f71ea12
shanoo0093 <71050916+shanoo0093@users.noreply.github.com>
2020-09-10 11:44:07 +0800

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>首頁</title>
+ </head>
+ <body>
+<?php session_start(); ?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
+include("mysql_connect.inc.php");
