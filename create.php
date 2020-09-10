854009b0d41da0098919b3c20cc0a7eb63a6a79c
shanoo0093 <71050916+shanoo0093@users.noreply.github.com>
2020-09-10 15:00:46 +0800

    Update create.php

--- create.php
+++ create.php
@@ -13,7 +13,10 @@ if(isset($_POST['Submit'])){
         $con -> query($sql);
         header("location:login.php");
         }
-             }
+ else{
+      echo '網頁錯誤'
+     }
+                     }
         $con -> close();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
